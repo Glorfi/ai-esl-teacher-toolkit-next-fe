44258a97f35d5c312dc7a48ea713382e98df41b9
Konstantin Bobkov <mrglorf@gmail.com>
2024-06-02 11:21:37 +0400

    fix suspense ref ESL-58

--- src/app/auth/magic/page.tsx
+++ src/app/auth/magic/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { MagicAuthResolver } from '@/widgets/user';
 import { Stack, Text } from '@chakra-ui/react';
+import { Suspense } from 'react';
 
 const MagicPage = (): JSX.Element => {
   return (
     <Stack minH={'100vh'} alignItems={'center'} justifyContent={'center'}>
-      <MagicAuthResolver />
+      <Suspense>
+        <MagicAuthResolver />
+      </Suspense>
     </Stack>
   );
 };
