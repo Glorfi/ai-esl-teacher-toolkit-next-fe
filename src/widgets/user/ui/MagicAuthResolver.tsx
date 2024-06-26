519959c093492f97f9a6d40630c535b717c292f8
Glorfi <119957768+Glorfi@users.noreply.github.com>
2024-06-26 18:48:25 +0400

    Esl 58 single sign on fe implementation (#14)
    * redesigned tabs ref ESL-48
    
    * add inputTheme and custom input component ref ESL-48
    
    * new sentence edit form almost ready ref ESL-48
    
    * new sentence form almost ready ref ESL-48
    
    * add redesigned ex component ref ESL-48
    
    * fix styles of edit exercise and ex component ref ESL-48
    
    * sentence form ready ref ESL-48
    
    * updated thumbnail and recent exercise box ref ESL-48
    
    * sevendays list fix ref ESL-48
    
    * upd DeleteEx PopUp and Mobile Menu Icons fix ref ESL-48
    
    * upd Share Ex PopUp ref ESL-48
    
    * redesigned AddTopicMenu ref ESL-48
    
    * add new signon widget ref ESL-48
    
    * resolver redesigned ref ESL-58

--- src/widgets/user/ui/MagicAuthResolver.tsx
+++ src/widgets/user/ui/MagicAuthResolver.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { addExerciseList } from '@/entities/exercise';
+import { customError } from '@/shared/constants/customError';
 import { setUser } from '@/entities/user';
 import {
   useLazyGetCurrentUserQuery,
@@ -8,7 +9,9 @@ import {
 import { APP_PATHS } from '@/shared';
 import { LSHandler } from '@/shared/hooks/handleLocalStorage';
 import { useAppDispatch, useAppSelector } from '@/shared/hooks/hooks';
-import { Spinner, Text } from '@chakra-ui/react';
+import { BookIcon } from '@/shared/ui/icons/BookIcon';
+import { Link } from '@chakra-ui/next-js';
+import { Box, Button, HStack, Spinner, Text, VStack } from '@chakra-ui/react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
@@ -18,14 +21,18 @@ export const MagicAuthResolver = (): JSX.Element => {
   const user = useAppSelector((state) => state.user);
   const emailParam = queryParams.get('email');
   const tokenParam = queryParams.get('token');
-  
+
   const email = emailParam ? emailParam.replace(/ /g, '+') : null;
   const token = tokenParam ? tokenParam.replace(/ /g, '+') : null;
   const router = useRouter();
   const [login, logInResponse] = useMagicAuthMutation();
   const [auth, authResponse] = useLazyGetCurrentUserQuery();
   const [isRendered, setIsRendered] = useState<boolean>(false);
+  const [errorCode, setErrorCode] = useState<
+    string | number | null | undefined
+  >(null);
 
+  
   useEffect(() => {
     !isRendered ? setIsRendered(true) : false;
   }, []);
@@ -53,29 +60,76 @@ export const MagicAuthResolver = (): JSX.Element => {
       router.push(APP_PATHS.DASHBOARD);
     }
   }, [user]);
+
+  useEffect(() => {
+    if (logInResponse.error) {
+      const customError = logInResponse.error as customError;
+      setErrorCode(customError.data.code);
+    }
+  }, [logInResponse.error]);
   return (
-    <>
+    <VStack mt={'calc(100vh / 5)'}>
+      <HStack minH={'24px'}>
+        <BookIcon color={'primary.base'} />
+        <Text
+          fontFamily={'alt'}
+          textTransform={'uppercase'}
+          fontSize={'md'}
+          fontWeight={'bold'}
+        >
+          ESL Teacher ToolKit
+        </Text>
+      </HStack>
       {!email || !token ? (
-        <Text>Token or email is absent in the link</Text>
+        <>
+          <Text mt={'86px'} fontSize={['xl', '2xl']} fontWeight={'600'}>
+            Token or email is absent in the link
+          </Text>
+          <Link href={APP_PATHS.SIGN_IN} mt={'36px'}>
+            <Button variant={'solid'} colorScheme="primary">
+              Back to login
+            </Button>
+          </Link>
+        </>
       ) : null}
       {logInResponse.isLoading || authResponse.isLoading ? (
-        <>
+        <VStack mt={'86px'} w={'100%'}>
           <Spinner
             thickness="4px"
             size={'xl'}
             speed="0.8s"
             emptyColor="gray.200"
-            color={'secondary.base'}
+            color={'primary.base'}
             m={'0 auto'}
           />
-          <Text>Logging in...</Text>
-        </>
+          <Text
+            fontSize={['xl', '2xl']}
+            fontWeight={'600'}
+            textAlign={'center'}
+          >
+            The page is loading, please wait a couple of seconds...
+          </Text>
+        </VStack>
       ) : null}
       {logInResponse.isError && (
-        <Text color={'error.base'} fontWeight={'bold'}>
-          Something went wrong with a token
-        </Text>
+        <>
+          <Text
+            mt={'86px'}
+            fontSize={['xl', '2xl']}
+            fontWeight={'600'}
+            textAlign={'center'}
+          >
+            {errorCode === 400
+              ? 'Invalid verification token. Please try again.'
+              : 'Something went wrong. Please try again'}
+          </Text>
+          <Link href={APP_PATHS.SIGN_IN} mt={'36px'}>
+            <Button variant={'solid'} colorScheme="primary">
+              Back to login
+            </Button>
+          </Link>
+        </>
       )}
-    </>
+    </VStack>
   );
 };
