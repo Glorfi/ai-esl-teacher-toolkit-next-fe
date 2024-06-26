c9cb83a205ac290f9abf8d35a2a97df807a79c24
Konstantin Bobkov <mrglorf@gmail.com>
2024-06-26 10:28:49 +0400

    add new signon widget ref ESL-48

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -6,8 +6,7 @@ import { Stack, Text } from '@chakra-ui/react';
 const LoginPage = (): JSX.Element => {
   return (
     <Stack minH={'100vh'} alignItems={'center'} justifyContent={'center'}>
-      <Text>Login to your account</Text>
-      <SignonWidget />
+      <SignonWidget type="signin" />
     </Stack>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,71 @@
 'use client';
-import { VStack, Text } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { VStack, Text, Button, ButtonGroup, Link } from '@chakra-ui/react';
 import { Header } from '@/widgets/header';
+import { APP_PATHS } from '@/shared';
 
 export default function Home() {
   return (
     <VStack
       minHeight={'100vh'}
       m={'0 auto'}
-      maxW={'800px'}
+      maxW={'1200px'}
       padding={['0 20px', '0 20px', '0 20px', '0']}
     >
-      <Header />
+      {/* <Header /> */}
       <VStack
         gap={0}
-        justifyContent={'flex-start'}
-        alignItems={'flex-start'}
-        mt={'80px'}
+        minHeight={'100vh'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        // mt={'80px'}
         w={'100%'}
       >
-        <Text color={'primary.base'} fontSize={'24px'}>
+        {/* <Text color={'primary.base'} fontSize={'24px'}>
           Welcome to
-        </Text>
+        </Text> */}
         <Text
           as="h1"
           color={'primary.base'}
-          fontSize={'80px'}
+          fontSize={'64px'}
           fontWeight={'bold'}
-          lineHeight={'80px'}
+          //  lineHeight={'80px'}
+          textAlign={'center'}
         >
-          ESL Teacher ToolKit
+          ESL Teacher ToolKit is your AI assistant for creating exercises
         </Text>
+        <Text
+          mt={'24px'}
+          as="h2"
+          color={'primary.base'}
+          fontSize={'20px'}
+          fontWeight={'bold'}
+          //  lineHeight={'80px'}
+          textAlign={'center'}
+        >
+          Save time, enhance lesson quality, and inspire your students with new,
+          unique assignments.
+        </Text>
+        <ButtonGroup alignItems={'center'} mt={'46px'}>
+          <Link as={NextLink} href={APP_PATHS.SIGN_UP}>
+            <Button
+              variant={'solid'}
+              colorScheme="primary"
+              //borderRadius={'40px'}
+            >
+              Sign up
+            </Button>
+          </Link>
+          <Link as={NextLink} href={APP_PATHS.SIGN_IN}>
+            <Button
+              variant={'outline'}
+              colorScheme="primary"
+              //borderRadius={'40px'}
+            >
+              Log in
+            </Button>
+          </Link>
+        </ButtonGroup>
       </VStack>
     </VStack>
   );
