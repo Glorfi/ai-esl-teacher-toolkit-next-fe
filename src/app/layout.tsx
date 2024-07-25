47d991f9a2a309c53b1e2957bc91e31d20be94a9
Konstantin Bobkov <mrglorf@gmail.com>
2024-07-25 06:40:28 +0400

    opengraph test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,28 @@ import { AuthComponent } from '@/widgets/user';
 
 export const metadata: Metadata = {
   title: 'AI ESL Teacher Toolkit',
-  description: 'AI ESL Teacher Toolkit is great!',
+  description:
+    'Create customizable English exercises, activities, and worksheets using AI ESL Teacher Toolkit. Tailored for teachers and students of all levels.',
+  keywords: [
+    'AI English exercises',
+    'English teaching materials',
+    'esl resources for teachers',
+    'esl teaching materials',
+    'teaching english as a second language resources',
+    'esol teaching materials',
+    'esl english teaching resources',
+    'english language teaching resources',
+    'teaching resources for english teachers',
+    'esl resources for teachers free',
+    'esl teaching materials free',
+    'english teaching materials',
+    'teaching materials for english teachers',
+    'free english teaching resources',
+    'free english materials for teachers',
+    'ai tools for teachers',
+    'ai teaching tools',
+    'chat gpt for teachers'
+  ],
 };
 
 export default function RootLayout({
